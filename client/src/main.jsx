07d8def5c174d73b4f252393324d043143a32cd3
Ankit Das <ecryptoguru@gmail.com>
2023-12-09 19:23:25 +0530

    Update9

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Home from './page/Home';
-import './index.css';
+import { CreateBattle, JoinBattle, Battleground, Battle, Home } from "./page";
+import { OnboardModal } from "./components";
+import { GlobalContextProvider } from "./context";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </BrowserRouter>,
+    <GlobalContextProvider>
+      <OnboardModal />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/battleground" element={<Battleground />} />
+        <Route path="/create-battle" element={<CreateBattle />} />
+        <Route path="/join-battle" element={<JoinBattle />} />
+        <Route path="/battle/:battleName" element={<Battle />} />
+      </Routes>
+    </GlobalContextProvider>
+  </BrowserRouter>
 );
