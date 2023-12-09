e8331381b7497f7ab1b525df49681c27965d5ed3
Ankit Das <ecryptoguru@gmail.com>
2023-12-09 12:31:04 +0530

    Update4

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,13 +1,81 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { CustomButton, CustomInput, PageHOC } from "../components";
+import { useGlobalContext } from "../context";
 
 const Home = () => {
+  const { contract, walletAddress, gameData, setShowAlert, setErrorMessage } =
+    useGlobalContext();
+  const [playerName, setPlayerName] = useState("");
+  const navigate = useNavigate();
+
+  const handleClick = async () => {
+    try {
+      const playerExists = await contract.isPlayer(walletAddress);
+
+      if (!playerExists) {
+        await contract.registerPlayer(playerName, playerName, {
+          gasLimit: 500000,
+        });
+
+        setShowAlert({
+          status: true,
+          type: "info",
+          message: `${playerName} is being summoned!`,
+        });
+
+        setTimeout(() => navigate("/create-battle"), 8000);
+      }
+    } catch (error) {
+      setErrorMessage(error);
+    }
+  };
+
+  useEffect(() => {
+    const createPlayerToken = async () => {
+      const playerExists = await contract.isPlayer(walletAddress);
+      const playerTokenExists = await contract.isPlayerToken(walletAddress);
+
+      if (playerExists && playerTokenExists) navigate("/create-battle");
+    };
+
+    if (contract) createPlayerToken();
+  }, [contract]);
+
+  useEffect(() => {
+    if (gameData.activeBattle) {
+      navigate(`/battle/${gameData.activeBattle.name}`);
+    }
+  }, [gameData]);
+
   return (
-    <div>
-      <h1 className="text-5xl p-3">FusionwaveFight</h1>
-      <h2 className="text-3xl p-3">Web3 NFT Battle-style Card Game</h2>
-      <p className="text-xl p-3">Made with 💜 by Ankit Das</p>
-    </div>
+    walletAddress && (
+      <div className="flex flex-col">
+        <CustomInput
+          label="Name"
+          placeHolder="Enter your player name"
+          value={playerName}
+          handleValueChange={setPlayerName}
+        />
+
+        <CustomButton
+          title="Register"
+          handleClick={handleClick}
+          restStyles="mt-6"
+        />
+      </div>
+    )
   );
 };
 
-export default Home;
+export default PageHOC(
+  Home,
+  <>
+    Welcome to FusionFight <br /> a Web3 Multiplayer Game
+  </>,
+  <>
+    Connect your wallet to start playing <br /> the ultimate Web3 NFT Battle
+    Card Game
+  </>
+);
